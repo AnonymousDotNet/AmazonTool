57c05e91ff79f1aebb24e9a180c24ad2e07fc999
HG888 <18776095145@163.com>
2025-06-24 21:24:54 +0800

    创建WPF项目

---
+++ AmazonTool.WPF/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AmazonTool.WPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
--- AmazonTool.WPF/obj/Debug/net8.0-windows/AmazonTool.WPF.AssemblyInfo.cs
+++ AmazonTool.WPF/obj/Debug/net8.0-windows/AmazonTool.WPF.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("AmazonTool.WinForm")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("AmazonTool.WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6db45d0e3cfa82da2e1c8a1e926ad0dab7dbecf9")]
-[assembly: System.Reflection.AssemblyProductAttribute("AmazonTool.WinForm")]
-[assembly: System.Reflection.AssemblyTitleAttribute("AmazonTool.WinForm")]
+[assembly: System.Reflection.AssemblyProductAttribute("AmazonTool.WPF")]
+[assembly: System.Reflection.AssemblyTitleAttribute("AmazonTool.WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
